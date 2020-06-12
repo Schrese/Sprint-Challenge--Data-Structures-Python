3d091717112126ba1d565b57503009ee3927b9a8
Schrese <schreseholloway@gmail.com>
2020-06-12 18:04:08 -0400

    Finished ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,43 +3,97 @@ class RingBuffer:
         # pass
         self.capacity = capacity
         self.q = []
-        self.oldest_val = None
+        self.oldest_index = -1
+        self.size = 0
+        # self.oldest_val = None
         # Maybe I need to store the next value
         # self.next_val = None
-        self.size = 0
+
 
     def append(self, item):
         # pass
-        print(self.q)
+        print(self.get())
+        print('oldest index', self.oldest_index, self.size)
         # if the current size is not at capacity
-        if self.size == 0:
-            # add the item to the queue 
+        if self.size < self.capacity:
+            # add the item to the queue using insert
             self.q.insert(self.size, item)
-            # increment size
-            self.size = 1
-        # if the current size is 1 less than capacity
-        elif self.size < self.capacity:
-            # store the current first element as the oldest value
-            self.oldest_val = self.q[0]
-            # add the item to the queue
-            self.q.insert(self.size, item)
-            # increment size
+            # increment the size by 1
             self.size += 1
-        # if the current size is the same as capacity
+            # store the first element's index (0) as the oldest_index
+            self.oldest_index = 0
+        # otherwise, the current size is equal to or greater than capacity (but we're only working with equal to)
         else:
-            val_index = self.q.index(self.oldest_val)
-            # Refactoring for if the element to remove is the last one
-            if val_index == self.capacity:
-                self.q.remove(self.oldest_val)
-                self.q.insert(self.capacity-1, item)
-                self.oldest_val = self.q[0]
-                print('hello')
+            # update the oldest index to hold the item
+            self.q[self.oldest_index] = item            
+            # if the oldest index is equal to the capacity minus 1 (because of the 0 index)
+            if self.oldest_index == self.capacity - 1:
+                # # set the oldest index to be 0
+                self.oldest_index = 0
+            # otherwise the index will be between 0 and capacity
             else:
-                self.q.insert(self.q.index(self.oldest_val), item)
-                new_index = self.q.index(self.oldest_val)
-                self.q.remove(self.oldest_val)
-                oldest_val = self.q[new_index + 1]
-                print('there')
+                # self.q[self.oldest_index] = item
+                self.oldest_index += 1
+
+
+        # WAS DOING EXTRA WORK BELOW
+        # else:
+        #     # if the oldest index is equal to the capacity minus 1 (because of the 0 index)
+        #     self.q[self.oldest_index] = item
+        #     if self.oldest_index == self.capacity - 1:
+        #         # remove the value at that index
+        #         # del self.q[self.oldest_index]
+        #         # insert the new item at the next index
+        #         # self.q.insert(self.oldest_index, item)
+        #         # # set the oldest index to be 0
+        #         self.oldest_index = 0
+        #     # otherwise the index will be between 0 and capacity
+        #     else:
+        #         # remove the value at the next index   
+        #         # del self.q[self.oldest_index]             
+        #         # # insert the new item at that index
+        #         # self.q.insert(self.oldest_index, item)
+        #         # set new oldest index
+        #         # self.q[self.oldest_index] = item
+        #         self.oldest_index += 1
+
+
+# Old version where I was storing the oldest value. Above will be an attempt to store the correct index
+    # def append(self, item):
+    #     # pass
+    #     print(self.q)
+    #     # if the current size is not at capacity
+    #     if self.size == 0:
+    #         # add the item to the queue 
+    #         self.q.insert(self.size, item)
+    #         # increment size
+    #         self.size = 1
+    #     # if the current size is 1 less than capacity
+    #     elif self.size < self.capacity:
+    #         # store the current first element as the oldest value
+    #         self.oldest_val = self.q[0]
+    #         # add the item to the queue
+    #         self.q.insert(self.size, item)
+    #         # increment size
+    #         self.size += 1
+    #     # if the current size is the same as capacity
+    #     else:
+    #         val_index = self.q.index(self.oldest_val)
+    #         # Refactoring for if the element to remove is the last one
+    #         if val_index == self.capacity:
+    #             self.q.remove(self.oldest_val)
+    #             self.q.insert(self.capacity-1, item)
+    #             self.oldest_val = self.q[0]
+    #             print('hello')
+    #         else:
+    #             self.q.insert(self.q.index(self.oldest_val), item)
+    #             new_index = self.q.index(self.oldest_val)
+    #             self.q.remove(self.oldest_val)
+    #             oldest_val = self.q[new_index + 1]
+    #             print('there')
+
+
+
 
             # # find the index where oldest value was stored
             # old_index = self.q.index(self.oldest_val)
