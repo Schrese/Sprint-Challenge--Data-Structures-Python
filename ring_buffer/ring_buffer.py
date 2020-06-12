3051f066b674e4aa70005691463682e5656b652b
Schrese <schreseholloway@gmail.com>
2020-06-12 12:48:57 -0400

    Stuck on ring_buffer Will come back to this a bit later

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,12 +2,57 @@ class RingBuffer:
     def __init__(self, capacity):
         # pass
         self.capacity = capacity
+        self.q = []
+        self.oldest_val = None
+        # Maybe I need to store the next value
+        self.next_val = None
+        self.size = 0
 
     def append(self, item):
-        pass
+        # pass
+        # if the current size is not at capacity
+        if self.size == 0:
+            # add the item to the queue 
+            self.q.insert(self.size, item)
+            # increment size
+            self.size = 1
+        # if the current size is 1 less than capacity
+        elif self.size < self.capacity:
+            # store the current first element as the oldest value
+            self.oldest_val = self.q[0]
+            # add the item to the queue
+            self.q.insert(self.size, item)
+            # increment size
+            self.size += 1
+        # if the current size is the same as capacity
+        else:
+            # find the index where oldest value was stored
+            old_index = self.q.index(self.oldest_val)
+            # store the current next element
+            next_index = old_index + 1
+            print('old index', next_index)
+            # remove the oldest value
+            self.q.remove(self.oldest_val)
+            # change oldest value to next element in the queue
+            self.oldest_val = self.q[next_index]
+            # add the item to the queue
+            self.q.insert(old_index, item)
+            self.size = self.capacity
+        # print(item, self.size, self.oldest_val)
+
 
     def get(self):
-        pass
+        # pass
+        return self.q
+
+
+# Problems I ran into:
+# the first 4 tests run fine
+# BUT once the queue is lentgth of the capacity, only the first 2 indices are updated, the last 3 are un-touched
+
+
+# Realizations after I started:
+# I might need to store the current size in order to compare it to the capacity
 
 
 # Before I get started
